9c49284d495adb32bcebc4618a69b1592a78d545
EhabMohamed <ehab.mo.work@gmail.com>
2026-07-24 19:06:03 +0300

    creates style of text and text Best seller

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utility/style_txt.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_books.dart';
 import 'package:flutter/material.dart';
@@ -11,10 +12,13 @@ class HomeViewBody extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 22),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 25),
             CustomAppBar(),
             CustomListViewBooks(),
+            SizedBox(height: 40),
+            Text('Best seller', style: StyleTxt.titleMedium),
           ],
         ),
       ),
